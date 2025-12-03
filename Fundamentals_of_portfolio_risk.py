4db86fba0a6f2a9fe2a9513a220d2ea73fdce36a
Beatriz Lima <152665224+bealima005@users.noreply.github.com>
2025-12-03 00:09:49 +0000

    Update Fundamentals_of_portfolio_risk_upd1.py
    change description
    make the code clear

--- Fundamentals_of_portfolio_risk.py
+++ Fundamentals_of_portfolio_risk.py
@@ -1,36 +1,31 @@
-#Python code to compute the daily returns in percentage, of Dow Stocks listed in Sec.1.1
-#calls function StockReturnsComputing to compute asset returns
+#Python code to compute the daily returns in percentage, of Apple Inc. Common Stock (AAPL)
+#calls function Stock_Returns_Computing to compute asset returns
 
 #dependencies
 import numpy as np
 import pandas as pd
 
 #input stock prices dataset
-#'/kaggle/input/fundamentals-of-risk-and-return-of-a-portfolio/DJIA_Apr112014_Apr112019.csv'
 stockfile = 'C:/Users/Utilizador/Desktop/Patinha Parreca/Projeto/HistoricalData_apple.csv'
 
-#lê o ficheiro csv e coloca o por colunas 
+#reads the CSV file and turns it into a table - dataframe(df)
 df = pd.read_csv(stockfile)
 
-#imprime todo o dataframe em formato de tabela: linha e coluna
+#Print all the dataframe
 print(df)
 
-#imprime uma coluna especifica 
-print(df['Close/Last'])
+#chooses a specific column, in this case, the one referring to the close/last stock price
+cl = df['Close/Last']
 
-#substitui o $ por nada e torna o um float
-close_price = df['Close/Last'].replace(r'\$','', regex = True).astype(float)
+#deletes $, by replacing it with a blank space, and turns the stock prices, originally strings, into floats
+close_price = cl.replace(r'\$','', regex = True).astype(float)
 print(close_price)
-
-"""for i in close_price:
-    print(i)"""
 
 
 #the return of a stock buy on day x and sell on day y>x, can be achieved
 # calculating the rate of return Rt (%) = ((Py-Px)/Px)*100
-# in other words, the dayly return of a stock is Rt% for the day concerned
+# in other words, the daily return of a stock is Rt% for the day concerned
 
-#function to compute asset returns
 def Stocks_Return_Computing (close_price, rows):
     """computes the daily returns of stocks, 
        ((today-yesterday)/yesterday)*100,
@@ -44,6 +39,7 @@ def Stocks_Return_Computing (close_price, rows):
     return stock_return
 
 print(Stocks_Return_Computing(close_price= close_price, rows=20))
+
 
 
 
